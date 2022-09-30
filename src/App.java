391d3c51b6e325ffdb639408c4a1eb953537760b
Piripa <viniaoliveira22@gmail.com>
2022-09-30 12:02:40 -0300

    Atualização

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
+
 import java.util.HashMap;
 import java.util.Scanner;
-
-public class App{
+public class App {
   public static void main(String[] args) {
-    
+  
     Scanner input = new Scanner(System.in);
     Dicionario leitor = new Dicionario();
     HashMap<String, Personagem> recebePersonagem = leitor.LeitorPersonagem("rsc/DicPersonagem.txt");
@@ -16,5 +16,4 @@ public class App{
 
     input.close();
   }
-
 }
